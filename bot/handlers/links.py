ccd3a6455c3779b5baafd8aee3992a58ebcb1084
Zelefin <nagangoroh@gmail.com>
2023-11-13 15:47:44 +0200

    added links phrases

--- bot/handlers/links.py
+++ bot/handlers/links.py
@@ -6,6 +6,7 @@ from aiogram.fsm.context import FSMContext
 
 from bot.middlewares import LinksMessageMiddleware
 from bot.db.db_functions import add_link_to_lesson, remove_link_to_lesson, get_links_list
+from bot.phrases import links_phrases
 
 
 class SetLink(StatesGroup):
@@ -27,7 +28,7 @@ links_router.message.middleware(LinksMessageMiddleware())
 
 @links_router.message(Command("set_link"))
 async def command_mailing_handler(message: Message, state: FSMContext) -> None:
-    await message.answer(text="Очікую назву предмету.\nВона має бути ідентичною як в jetiq!")
+    await message.answer(text=links_phrases['waiting_lesson_name'])
     await state.set_state(SetLink.sending_lesson)
 
 
@@ -36,7 +37,7 @@ async def lesson_taker(message: Message, state: FSMContext):
 
     lesson_name = message.text
 
-    await message.answer(text="Тепер надішліть тип уроку (ЛК, ПЗ, ЛБ)")
+    await message.answer(text=links_phrases['waiting_lesson_type'])
     await state.update_data(lesson_name=lesson_name)
     await state.set_state(SetLink.sending_lesson_type)
 
@@ -46,8 +47,7 @@ async def lesson_taker(message: Message, state: FSMContext):
 
     lesson_type = message.text
 
-    await message.answer(text="Тепер надішліть прізвище та ініціали викладача.\n\nНаприклад:\n\n"
-                         "Кабачій В.В.\nСтахов О.Я.\nКруподьорова Л.М.\n\nТак, крапка у кінці обов'язкова.")
+    await message.answer(text=links_phrases['waiting_teachers_name'])
     await state.update_data(lesson_type=lesson_type.upper())
     await state.set_state(SetLink.sending_teacher_short_name)
 
@@ -57,18 +57,8 @@ async def lesson_taker(message: Message, state: FSMContext):
 
     teacher_short_name = message.text
 
-    await message.answer(text="Тепер надішліть посилання")
+    await message.answer(text=links_phrases['waiting_link'])
     await state.update_data(teacher_short_name=teacher_short_name)
-    await state.set_state(SetLink.sending_link)
-
-
-@links_router.message(SetLink.sending_lesson)
-async def lesson_taker(message: Message, state: FSMContext):
-
-    lesson_link = message.text
-
-    await message.answer(text="Тепер надішліть посилання")
-    await state.update_data(lesson_link=lesson_link)
     await state.set_state(SetLink.sending_link)
 
 
@@ -91,17 +81,19 @@ async def sending_link(message: Message, state: FSMContext, session_maker, presi
                                          lesson_type=lesson_type,
                                          teacher_short_name=teacher_short_name,
                                          lesson_link=link)
-                await message.answer(text=f"Тепер предмет: {lesson_name}, {lesson_type}\n\n"
-                                          f"Викладача '{teacher_short_name}'\nМає посилання: {message.text}")
+                await message.answer(text=links_phrases['added_link'].format(n=lesson_name,
+                                                                             type=lesson_type,
+                                                                             teacher=teacher_short_name,
+                                                                             l=message.text))
                 await state.clear()
                 return
 
-    await message.answer(text="Я очікую посилання!")
+    await message.answer(text=links_phrases['link_only'])
 
 
 @links_router.message(Command("delete_link"))
 async def link_remove(message: Message, state: FSMContext):
-    await message.answer(text="Надішліть мені назву предмету який бажаєте видалити")
+    await message.answer(text=links_phrases['delete_name'])
     await state.set_state(RemoveLink.lesson_to_remove)
 
 
@@ -110,7 +102,7 @@ async def link_remove(message: Message, state: FSMContext):
 
     lesson_name = message.text
 
-    await message.answer(text="Тепер надішліть тип уроку (ЛК, ПЗ, ЛБ)")
+    await message.answer(text=links_phrases['waiting_lesson_type'])
     await state.update_data(lesson_name=lesson_name)
     await state.set_state(RemoveLink.lesson_type_to_remove)
 
@@ -120,7 +112,7 @@ async def link_remove(message: Message, state: FSMContext):
 
     lesson_type = message.text
 
-    await message.answer(text="Тепер надішліть прізвище та ініціали викладача.")
+    await message.answer(text=links_phrases['delete_teacher_name'])
     await state.update_data(lesson_type=lesson_type.upper())
     await state.set_state(RemoveLink.lesson_teacher_to_remove)
 
@@ -137,21 +129,23 @@ async def link_remove(message: Message, state: FSMContext, session_maker, presid
                                 lesson_name=lesson_name,
                                 lesson_type=lesson_type,
                                 teacher_short_name=teacher_short_name)
-    await message.answer(text=f"Видалив посилання для предмету {lesson_name}, {lesson_type}"
-                              f" Викладача {teacher_short_name}\n\nПеревірте це командою <i>/links</i>")
+    await message.answer(text=links_phrases['deleted'].format(n=lesson_name,
+                                                              type=lesson_type,
+                                                              teacher=teacher_short_name))
     await state.clear()
 
 
 @links_router.message(Command("links"))
 async def get_links_list_command(message: Message, session_maker, president_group):
-    await message.answer(text="Усі уроки з посиланнями:\n\nЩоб видалити пропишіть команду /delete_link,"
-                              " а потім таку ж інформацію як в повідомленні!!")
+    await message.answer(text=links_phrases['all_links'])
 
     links_list = await get_links_list(session_maker, president_group)
 
     if links_list:
         for link in links_list:
-            await message.answer(text=f"Назва: {link[0]}\n\nТип: {link[1]}\n\nВикладач: {link[2]}\n\n"
-                                      f"Посилання: {link[3]}")
+            await message.answer(text=links_phrases['link_temp'].format(n=link[0],
+                                                                        type=link[1],
+                                                                        teacher=link[2],
+                                                                        l=link[3]))
     else:
-        await message.answer(text="Уроків з посиланням немає")
+        await message.answer(text=links_phrases['no_links'])
