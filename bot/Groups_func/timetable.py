670d9509817d3b7b18a7a1d4aa0dae0db7207580
Zelefin <nagangoroh@gmail.com>
2023-11-20 18:34:37 +0200

    header of the file now constructs in the function

--- bot/Groups_func/timetable.py
+++ bot/Groups_func/timetable.py
@@ -1,6 +1,6 @@
-from bot.days_weeks_stuff import cur_week
+from bot.days_weeks_stuff import cur_week, cur_day
+from ScrapItUp.ids_dict import groups_ids
 from pathlib import Path
-
 
 PATH = Path.cwd()
 
@@ -8,15 +8,24 @@ if PATH.name == "bot":
     PATH = PATH.parent
 
 
-async def send_lessons(user_info, day: int, week=cur_week()) -> str:
-
+async def send_lessons(user_info: dict, day: int = cur_day(), week: int = cur_week()) -> str:
     group = str(user_info['group'])  # group id
     subgroup = str(user_info['subgroup'])  # its integer already
+
+    # TODO: bro...
+    group_name = next((key for key, value in groups_ids.items() if value == group), None)
+
+    if subgroup != "0":
+        header = f"┌ {group_name}, {subgroup} Підгрупа\n"
+    else:
+        header = f"┌ {group_name}\n"
+        subgroup = "1"
 
     try:
         with open(str(PATH) + "/ScrapItUp/Groups/" + group + "/" +
                   str(week) + "/" + str(day) + "/IP" + subgroup + ".txt", "r", encoding="utf8") as text:
 
-            return text.read()
+            return header + text.read()
+
     except FileNotFoundError:
         return "Отакої! Файла не знайдено!😳\nНапишіть будь ласка про це у <i>/feedback</i>"
