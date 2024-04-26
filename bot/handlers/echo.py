cfa569b1f45ed2f583197eceea3bc0ff27df8f63
Zelefin <nagangoroh@gmail.com>
2024-04-26 18:51:48 +0300

    import only message type

--- bot/handlers/echo.py
+++ bot/handlers/echo.py
@@ -1,4 +1,5 @@
-from aiogram import types, Router, F
+from aiogram import Router, F
+from aiogram.types import Message
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.utils.markdown import hcode
@@ -7,14 +8,14 @@ echo_router = Router()
 
 
 @echo_router.message(F.text, StateFilter(None))
-async def bot_echo(message: types.Message):
+async def bot_echo(message: Message):
     text = ["Ехо без стану.", "Повідомлення:", message.text]
 
     await message.answer("\n".join(text))
 
 
 @echo_router.message(F.text)
-async def bot_echo_all(message: types.Message, state: FSMContext):
+async def bot_echo_all(message: Message, state: FSMContext):
     state_name = await state.get_state()
     text = [
         f"Ехо у стані {hcode(state_name)}",
