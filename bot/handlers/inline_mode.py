5e48ed96c7daa80524f9a4eb28fdc4ba4af9a468
Zelefin <nagangoroh@gmail.com>
2024-04-26 22:40:29 +0300

    better inline mode

--- bot/handlers/inline_mode.py
+++ bot/handlers/inline_mode.py
@@ -1,4 +1,4 @@
-from aiogram import Router, Bot
+from aiogram import Router, Bot, F
 from aiogram.types import (
     InlineQuery,
     InlineQueryResultArticle,
@@ -11,24 +11,27 @@ from infrastructure.vntu_timetable_api import VntuTimetableApi
 inline_mode_router = Router()
 
 
-@inline_mode_router.inline_query()
+@inline_mode_router.inline_query(F.query.regexp(r"^\d+\_\d+$"))
 async def handle_inline_query(
     inline_query: InlineQuery, bot: Bot, api: VntuTimetableApi
 ):
+    faculty_id, group_id = [int(value) for value in inline_query.query.split("_")]
     bot_info = await bot.get_me()
     response, faculties = await api.get_faculties()
-    faculty_id, group_id = inline_query.query.split("_")
+    if response != 200:
+        return
+
     group_name = None
     for faculty in faculties["data"]:
-        if faculty["id"] == int(faculty_id):
+        if faculty["id"] == faculty_id:
             for group in faculty["groups"]:
-                if group["id"] == int(group_id):
+                if group["id"] == group_id:
                     group_name = group["name"]
                     break
     if group_name:
         answer = [
             InlineQueryResultArticle(
-                id=faculty_id,
+                id="share_article",
                 title="Натисніть тут щоб поділитися!",
                 description=f"Поділіться зручним web app для перегляду розкладу групи {group_name}",
                 input_message_content=InputTextMessageContent(
@@ -36,7 +39,7 @@ async def handle_inline_query(
                     f"<a href='https://t.me/{bot_info.username}/timetable?startapp={faculty_id}_{group_id}'>"
                     f"{group_name}</a></b>"
                 ),
-                reply_markup=share_button(),
+                reply_markup=share_button(faculty_id=faculty_id, group_id=group_id),
                 thumbnail_url="https://vntu.edu.ua/projects/brandbook/logo11.png",
             )
         ]
