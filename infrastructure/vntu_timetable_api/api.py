d8f90551c027c712808b35f9f2f50bb94e536c67
Zelefin <nagangoroh@gmail.com>
2024-04-26 18:49:22 +0300

    get faculties method

--- infrastructure/vntu_timetable_api/api.py
+++ infrastructure/vntu_timetable_api/api.py
@@ -2,13 +2,9 @@ from infrastructure.vntu_timetable_api.base import BaseClient
 
 
 class VntuTimetableApi(BaseClient):
-    def __init__(self, api_key: str, **kwargs):
-        self.api_key = api_key
-        self.base_url = "https://some-api.com"
+    def __init__(self):
+        self.base_url = "https://vm4625529.25ssd.had.wf"
         super().__init__(base_url=self.base_url)
 
-    async def get_something(self, *args, **kwargs):
-        # await self._make_request(
-        #     ...
-        # )
-        return
+    async def get_faculties(self):
+        return await self._make_request(method="get", url="/v0/faculties")
