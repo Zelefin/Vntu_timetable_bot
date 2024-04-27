329673e4155361aa8e6a58ce4d7f8a2c7c3cb4df
Zelefin <nagangoroh@gmail.com>
2024-04-27 23:31:51 +0300

    new keyboard for inline view

--- bot/keyboards/user.py
+++ bot/keyboards/user.py
@@ -1,4 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from bot.misc.callback_data import InlineCallbackFactory
+from bot.misc.current_date import current_week
+
+days = ["🔴", "Пн", "Вт", "Ср", "Чт", "Пт"]
 
 
 def start_keyboard(reg: bool):
@@ -6,7 +12,7 @@ def start_keyboard(reg: bool):
         inline_keyboard=[
             [
                 InlineKeyboardButton(
-                    text="Оновити" if reg else "Зареєструватись",
+                    text="Оновити🔄" if reg else "Зареєструватись✍",
                     callback_data="reg_or_upd",
                 )
             ]
@@ -27,6 +33,52 @@ def subgroups_keyboard():
 def share_button(faculty_id: int | str, group_id: int | str):
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="Поділитися", switch_inline_query=f"{faculty_id}_{group_id}")]
+            [
+                InlineKeyboardButton(
+                    text="Поділитися", switch_inline_query=f"{faculty_id}_{group_id}"
+                )
+            ]
         ]
     )
+
+
+def inline_timetable_keyboard(day: int, week: str):
+    kb = InlineKeyboardBuilder()
+    cur_week = current_week()
+    kb.button(
+        text=days[1 if day != 0 else 0],
+        callback_data=InlineCallbackFactory(day=0, week=week),
+    )
+    kb.button(
+        text=days[2 if day != 1 else 0],
+        callback_data=InlineCallbackFactory(day=1, week=week),
+    )
+    kb.button(
+        text=days[3 if day != 2 else 0],
+        callback_data=InlineCallbackFactory(day=2, week=week),
+    )
+    kb.button(
+        text=days[4 if day != 3 else 0],
+        callback_data=InlineCallbackFactory(day=3, week=week),
+    )
+    kb.button(
+        text=days[5 if day != 4 else 0],
+        callback_data=InlineCallbackFactory(day=4, week=week),
+    )
+
+    kb.button(
+        text="Сьогодні🌆", callback_data=InlineCallbackFactory(day=-1, week=cur_week)
+    )
+    kb.button(
+        text="Завтра🏙", callback_data=InlineCallbackFactory(day=-2, week=cur_week)
+    )
+
+    kb.button(
+        text="➡Наступний тиждень" if week == cur_week else "⬇Цей тиждень",
+        callback_data=InlineCallbackFactory(
+            day=day, week="firstWeek" if week == "secondWeek" else "secondWeek"
+        ),
+    )
+
+    kb.adjust(5, 2, 1)
+    return kb.as_markup()
