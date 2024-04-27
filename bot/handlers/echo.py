0fa9974a414e0a9df1c72fed56c2c7154890d6dd
Zelefin <nagangoroh@gmail.com>
2024-04-27 16:33:34 +0300

    echoing only for callback

--- bot/handlers/echo.py
+++ bot/handlers/echo.py
@@ -1,25 +1,9 @@
-from aiogram import Router, F
-from aiogram.types import Message
-from aiogram.filters import StateFilter
-from aiogram.fsm.context import FSMContext
-from aiogram.utils.markdown import hcode
+from aiogram import Router
+from aiogram.types import CallbackQuery
 
 echo_router = Router()
 
 
-@echo_router.message(F.text, StateFilter(None))
-async def bot_echo(message: Message):
-    text = ["Ехо без стану.", "Повідомлення:", message.text]
-
-    await message.answer("\n".join(text))
-
-
-@echo_router.message(F.text)
-async def bot_echo_all(message: Message, state: FSMContext):
-    state_name = await state.get_state()
-    text = [
-        f"Ехо у стані {hcode(state_name)}",
-        "Зміст повідомлення:",
-        hcode(message.text),
-    ]
-    await message.answer("\n".join(text))
+@echo_router.callback_query()
+async def answer_query(callback: CallbackQuery):
+    await callback.answer(show_alert=True, text="Стара версія бота!")
