02e369ec4306068d7846bb16a66a21b114885761
Zelefin <nagangoroh@gmail.com>
2024-04-30 18:13:27 +0300

    handling exceptions and returning status 0 and empty dict in case of exception

--- infrastructure/vntu_timetable_api/api.py
+++ infrastructure/vntu_timetable_api/api.py
@@ -1,3 +1,5 @@
+import logging
+
 from infrastructure.vntu_timetable_api.base import BaseClient
 
 
@@ -7,7 +9,15 @@ class VntuTimetableApi(BaseClient):
         super().__init__(base_url=self.base_url)
 
     async def get_faculties(self):
-        return await self._make_request(method="get", url="/v0/faculties")
+        try:
+            return await self._make_request(method="get", url="/v0/faculties")
+        except Exception as e:
+            logging.error(e)
+            return 0, {}
 
     async def get_group_timetable(self, group_id: int):
-        return await self._make_request(method="get", url=f"/v0/groups/{group_id}")
+        try:
+            return await self._make_request(method="get", url=f"/v0/groups/{group_id}")
+        except Exception as e:
+            logging.error(e)
+            return 0, {}
