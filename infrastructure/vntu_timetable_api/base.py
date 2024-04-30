67fff200a2eb833d4924ee298779d11ca744f132
Zelefin <nagangoroh@gmail.com>
2024-04-30 18:13:48 +0300

    less max_time

--- infrastructure/vntu_timetable_api/base.py
+++ infrastructure/vntu_timetable_api/base.py
@@ -40,7 +40,7 @@ class BaseClient:
     @backoff.on_exception(
         backoff.expo,
         ClientError,
-        max_time=60,
+        max_time=2.5,
     )
     async def _make_request(
         self,
