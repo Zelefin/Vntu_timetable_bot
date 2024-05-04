784c4b5c93c07b20a59d81c2f4e7c35474ad26f8
Zelefin <nagangoroh@gmail.com>
2024-05-04 21:29:11 +0300

    closing session

--- infrastructure/vntu_timetable_api/api.py
+++ infrastructure/vntu_timetable_api/api.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 from infrastructure.vntu_timetable_api.base import BaseClient
 
@@ -8,16 +9,22 @@ class VntuTimetableApi(BaseClient):
         self.base_url = "https://vm4625529.25ssd.had.wf"
         super().__init__(base_url=self.base_url)
 
-    async def get_faculties(self):
+    async def get_faculties(self) -> tuple[int, dict[str, Any]]:
         try:
-            return await self._make_request(method="get", url="/v0/faculties")
+            status, faculties = await self._make_request(method="get", url="/v0/faculties")
+            await self.close()
+            return status, faculties
         except Exception as e:
             logging.error(e)
+            await self.close()
             return 0, {}
 
     async def get_group_timetable(self, group_id: int):
         try:
-            return await self._make_request(method="get", url=f"/v0/groups/{group_id}")
+            status, group_timetable = await self._make_request(method="get", url=f"/v0/groups/{group_id}")
+            await self.close()
+            return status, group_timetable
         except Exception as e:
             logging.error(e)
+            await self.close()
             return 0, {}
