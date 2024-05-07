67aae19b4bde0c6da966fe08b3bc4594b5433b9a
Zelefin <nagangoroh@gmail.com>
2024-05-07 16:00:37 +0300

    added dockstrings and changed base class functions

--- infrastructure/vntu_timetable_api/api.py
+++ infrastructure/vntu_timetable_api/api.py
@@ -5,20 +5,24 @@ from infrastructure.vntu_timetable_api.base import BaseClient
 
 
 class VntuTimetableApi(BaseClient):
+    """Class for making requests to VNTU Timetable API."""
+
     def __init__(self):
         self.base_url = "https://vm4625529.25ssd.had.wf"
         super().__init__(base_url=self.base_url)
 
     async def get_faculties(self) -> tuple[int, dict[str, Any]]:
+        """Method to get faculties data."""
         try:
-            return await self._make_request(method="get", url="/v0/faculties")
+            return await self.make_request(method="get", url="/v0/faculties")
         except Exception as e:
             logging.error(e)
             return 0, {}
 
     async def get_group_timetable(self, group_id: int) -> tuple[int, dict[str, Any]]:
+        """Method to get group timetable data."""
         try:
-            return await self._make_request(method="get", url=f"/v0/groups/{group_id}")
+            return await self.make_request(method="get", url=f"/v0/groups/{group_id}")
         except Exception as e:
             logging.error(e)
             return 0, {}
