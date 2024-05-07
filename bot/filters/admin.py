e056db3cff67e4a0a8b535eac8212e8279d0c971
Zelefin <nagangoroh@gmail.com>
2024-05-07 15:59:38 +0300

    simpler admin filter

--- bot/filters/admin.py
+++ bot/filters/admin.py
@@ -1,11 +1,9 @@
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
-from bot.config_reader import Config
-
 
 class AdminFilter(BaseFilter):
-    is_admin: bool = True
+    """Filter for admin commands."""
 
-    async def __call__(self, obj: Message, config: Config) -> bool:
-        return (obj.from_user.id == config.admin) == self.is_admin
+    async def __call__(self, obj: Message, admin_id: int) -> bool:
+        return obj.from_user.id == admin_id
